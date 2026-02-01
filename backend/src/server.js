67be3e5dc378da638974c6ef8cea8785db119bdb
sea <sea@ip-192-168-43-11.us-west-2.compute.internal>
2026-02-02 00:02:07 +0800

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 dotenv.config()
 
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
+
 const app = express();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth",authRoutes);
 app.use('/api/message',messageRoutes);
+
+// make ready for deployment
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../frontend','dist','index.html'));
+    })
+}
 
 app.listen(PORT,()=> {
     console.log(`Server is running on port ${PORT}`);
